c79e0a4cafb829807a94738aeda8edf5303ca063
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-02-02 20:15:03 +0530

    step2:Done with creating models, fake db, helper method

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// Model for Course - file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+// fake DB
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
 func main() {
 	fmt.Println("Welcome to Basic API Building")
 }
